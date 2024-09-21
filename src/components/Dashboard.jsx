cc6919668c91dd819da6257ff88c957bb78df3ad
Siddhesh Jagdale <siddheshjagdale3534@gmail.com>
2024-09-21 13:16:42 +0530

    Added heading

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,8 +4,14 @@ import Data from "./Data.jsx";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col md:flex-row h-[100vh] w-full justify-center items-center ">
-      <div className="flex flex-col md:flex-row h-[80vh] w-[90vw] sm:w-[80vw] shadow-lg">
+    <div className="flex flex-col h-[100vh] w-full justify-start items-center">
+      {/* h1 for Weatherly at the top */}
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-800 text-center mt-8">
+        Weatherly
+      </h1>
+
+      {/* Main dashboard container below the h1 */}
+      <div className="flex flex-col md:flex-row h-[80vh] w-[90vw] sm:w-[80vw] shadow-lg mt-4">
         <Image />
         <Data />
       </div>
